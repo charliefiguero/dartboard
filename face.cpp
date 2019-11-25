945b14166962bc73e1a4568ca77880177dc33b5f
Angus Parsonson <ap17691@it075779.wks.bris.ac.uk>
2019-11-25 17:03:38 +0000

    solo'd part a) of first page of report (ground truth and visualisation)

--- face.cpp
+++ face.cpp
@@ -24,6 +24,17 @@ void detectAndDisplay( Mat frame );
 String cascade_name = "frontalface.xml";
 CascadeClassifier cascade;
 
+// Ground truth faces array
+Rect dart4[] = {Rect(334, 102, 151, 166)};
+Rect dart5[] = {Rect(52, 139, 76, 73), Rect(45, 245, 79, 80), Rect(186, 200, 64, 85), Rect(250, 167, 56, 65),
+								Rect(294, 239, 56, 74), Rect(372, 185, 73, 69), Rect(429, 234, 53, 67), Rect(518, 177, 48, 64),
+								Rect(559, 246, 58, 67), Rect(646, 184, 59, 67), Rect(681, 242, 50, 71)};
+Rect dart6[] = {Rect(287, 116, 39, 42)};
+Rect dart7[] = {Rect(349, 186, 68, 95)};
+Rect dart9[] = {Rect(85, 206, 114, 140)};
+Rect dart11[] = {Rect(320, 80, 67, 69)};
+Rect dart13[] = {Rect(421, 125, 110, 129)};
+Rect dart14[] = {Rect(467, 220, 81, 99), Rect(723, 188, 101, 101)};
 
 /** @function main */
 int main( int argc, const char** argv )
@@ -53,16 +64,19 @@ void detectAndDisplay( Mat frame )
 	cvtColor( frame, frame_gray, CV_BGR2GRAY );
 	equalizeHist( frame_gray, frame_gray );
 
-	// 2. Perform Viola-Jones Object Detection 
+	// 2. Perform Viola-Jones Object Detection
 	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
-       // 3. Print number of Faces found
+  // 3. Print number of Faces found
 	std::cout << faces.size() << std::endl;
 
-       // 4. Draw box around faces found
-	for( int i = 0; i < faces.size(); i++ )
-	{
+  // 4. Draw box around faces found
+	for( int i = 0; i < faces.size(); i++ ) {
 		rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar( 0, 255, 0 ), 2);
 	}
+
+	// for ( int i = 0; i < sizeof(dart14)/sizeof(dart14[0]); i++) {
+	// 	rectangle(frame, Point(dart14[i].x, dart14[i].y), Point(dart14[i].x + dart14[i].width, dart14[i].y + dart14[i].height), Scalar( 0, 0, 255 ), 2);
+	// }
 
 }
